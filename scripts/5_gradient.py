542048bb5e54d52a2a095e4f960f9f591b8e8785
khanhptnk <nguyenxuankhanhm@gmail.com>
2017-03-29 14:27:01 -0400

    Change local minimum image

--- scripts/5_gradient.py
+++ scripts/5_gradient.py
@@ -11,11 +11,10 @@ pylab.plot([0],[0], marker='o', markersize=8, color='red')
 pylab.show() # show the plot
 
 
-x = np.linspace(-1.5,1.5,100) # 100 linearly spaced numbers
-y = x**4 - 2*x**2 + 1 # computing the values of sin(x)/x
+x = np.linspace(-2.5,1.5,100) # 100 linearly spaced numbers
+y = x**4 + x**3 - 2*x**2 + 1 # computing the values of sin(x)/x
 
 # compose plot
 pylab.plot(x,y) # sin(x)/x
-pylab.plot([-1],[0], marker='o', markersize=8, color='red')
-pylab.plot([1],[0], marker='o', markersize=8, color='red')
+pylab.plot([0.72],[0.60], marker='o', markersize=8, color='red')
 pylab.show() # show the plot
